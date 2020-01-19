d6ad671fa2eadec6a4dd9e376f23b6307f9fed46
King Elisha <ek.chibueze@gmail.com>
2020-01-19 16:16:03 +0100

    new route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Home from './components/routes/Home';
 
 function App() {
   return (
     <div style={{ padding: "3em 10vw" }}>
       <Router>
         <Navbar />
+
+        <Route path="/" component={Home} exacts />
       </Router>
     </div>
   );
