a86cceae06955bba0a7a596bc33009d864380a50
King Elisha <ek.chibueze@gmail.com>
2020-01-19 16:22:47 +0100

    new route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Home from './components/routes/Home';
+import Jobs from './components/routes/Jobs';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
 
         <Route path="/" component={Home} exacts />
+        <Route path="/jobs" component={Jobs} exacts />
       </Router>
     </div>
   );
---
+++ client/src/components/routes/Jobs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Jobs extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
