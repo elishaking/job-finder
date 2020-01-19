a86de30d8430e90beb63660d65b47f1c14b7992a
King Elisha <ek.chibueze@gmail.com>
2020-01-19 16:44:46 +0100

    display jobs

--- client/src/components/routes/Jobs.js
+++ client/src/components/routes/Jobs.js
@@ -9,14 +9,31 @@ export default class Jobs extends Component {
     fetch('/api/v1/jobs')
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        if (data.success) {
+          this.setState({ jobs: data.jobs });
+        }
       });
   }
 
   render() {
+    const { jobs } = this.state;
+
     return (
       <div>
         <h1>Jobs</h1>
+
+        <div className="jobs">
+          {
+            jobs.map((job, index) => (
+              <div key={index} className="job">
+                <h2>{job.title}</h2>
+                <small>${job.budget} &middot; {job.technologies}</small>
+                <p>{job.description}</p>
+                <small>{job.contactEmail}</small>
+              </div>
+            ))
+          }
+        </div>
       </div>
     )
   }
