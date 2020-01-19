e0a81135c2d495dbe7b9891b8ccacb3639fc5441
King Elisha <ek.chibueze@gmail.com>
2020-01-19 13:59:49 +0100

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('yarn init');
+const express = require('express');
+const path = require('path');
+
+const server = express();
+const PORT = process.env.PORT || 8000;
+
+server.get('/', (_, res) => {
+  res.send('root page');
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
