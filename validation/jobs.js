0f9c8a7ac1267d144d5c95b7d5f4eb0e6486f7b8
King Elisha <ek.chibueze@gmail.com>
2020-01-20 14:16:30 +0100

    addjob validation

---
+++ validation/jobs.js
@@ -0,0 +1,30 @@
+/**
+ * @description Validate new job request body
+ * @param {{title: string, technologies: string, budget: number, description: string, contactEmail: string}} data 
+ */
+const validateJobInput = (data) => {
+  const errors = {};
+
+  if (!data.title)
+    errors.title = "Please add a title";
+  else if (data.title.length > 200)
+    errors.title = "Title should be less than 200 characters";
+
+  if (!data.technologies)
+    errors.technologies = "Please add a technologies";
+  else if (data.technologies.length > 200)
+    errors.technologies = "Technologies should be less than 200 characters";
+
+  if (!data.budget)
+    errors.budget = "Please add a budget";
+  else if (data.budget.toString().length > 200)
+    errors.budget = "budget should be less than 200 characters";
+
+  if (!data.description)
+    errors.description = "Please add a description";
+
+  if (!data.contactEmail)
+    errors.contactEmail = "Please add a Contact Email";
+  else if (data.contactEmail.length > 200)
+    errors.contactEmail = "Contact Email should be less than 200 characters";
+};
