95d94aa0039607b6140f6fae08821b63dbfcd55c
King Elisha <ek.chibueze@gmail.com>
2020-01-27 20:34:48 +0100

    create new job integration test

--- test/integration/jobs.test.js
+++ test/integration/jobs.test.js
@@ -38,4 +38,40 @@ describe('Job Controller Integration Tests', () => {
       })
       .catch((err) => done(err));
   });
+
+  it('POST /api/v1/jobs - should create a new job', (done) => {
+    const newJob = {
+      title: 'Full Stack Engineer',
+      technologies: "React, MongoDB",
+      budget: 200000,
+      description: 'Build great projects',
+      contactEmail: 'mail@mail.com'
+    };
+
+    request(server)
+      .post('/api/v1/jobs')
+      .send(newJob)
+      .then((res) => {
+        expect(res.status).toEqual(201);
+        expect(res.body.success).toEqual(true);
+        expect(res.body.message).toEqual('Job Created');
+        expect(res.body).toHaveProperty('data');
+
+        const { data } = res.body;
+        expect(typeof data === 'object').toBe(true);
+        expect(data).toHaveProperty('title');
+        expect(data.title).toEqual(newJob.title);
+        expect(data).toHaveProperty('technologies');
+        expect(data.technologies).toEqual(newJob.technologies);
+        expect(data).toHaveProperty('budget');
+        expect(data.budget).toEqual(newJob.budget);
+        expect(data).toHaveProperty('description');
+        expect(data.description).toEqual(newJob.description);
+        expect(data).toHaveProperty('contactEmail');
+        expect(data.contactEmail).toEqual(newJob.contactEmail);
+
+        done();
+      })
+      .catch((err) => done(err));
+  });
 });
