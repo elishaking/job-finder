dbbff97daf9bfd2976ad02d8c67c02f67ebd1e2d
King Elisha <ek.chibueze@gmail.com>
2020-01-27 20:28:05 +0100

    search integration test

--- test/integration/jobs.test.js
+++ test/integration/jobs.test.js
@@ -19,4 +19,23 @@ describe('Job Controller Integration Tests', () => {
       })
       .catch((err) => done(err));
   });
+
+  it('GET /api/v1/jobs/search - should search for a location', (done) => {
+    request(server)
+      .get('/api/v1/jobs/search?term=React')
+      .then((res) => {
+        expect(res.status).toEqual(200);
+        expect(typeof res.body === 'object').toBe(true);
+
+        expect(res.body).toHaveProperty('data');
+        const { data } = res.body;
+        expect(Array.isArray(data)).toBe(true);
+
+        if (data.length > 0)
+          expect(data[0].technologies).toMatch(/React/);
+
+        done();
+      })
+      .catch((err) => done(err));
+  });
 });
