b4e6de3142fd7b73889fdad2bcc69f19d207bd42
King Elisha <ek.chibueze@gmail.com>
2020-01-27 18:55:37 +0100

    createJob test

--- test/unit/services/jobs.test.js
+++ test/unit/services/jobs.test.js
@@ -2,15 +2,44 @@ if (process.env.NODE_ENV !== 'CI') {
   const dotenv = require('dotenv');
   dotenv.config({ path: './test/config/config.env' });
 }
-const { findJobs } = require('../../../src/services/jobs');
+const { createJob, findJobs } = require('../../../src/services/jobs');
 
 describe('Job Service Unit tests', () => {
 
   it('findJobs() - should find all jobs in the database', (done) => {
     findJobs()
       .then(({ data }) => {
-        console.log(data);
         expect(Array.isArray(data)).toBe(true);
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('createJob() - should create a job in the database', (done) => {
+    const job = {
+      title: 'Full Stack Engineer',
+      technologies: "React, MongoDB",
+      budget: 200000,
+      description: 'Build great projects',
+      contactEmail: 'mail@mail.com'
+    };
+
+    createJob(job)
+      .then(({ data }) => {
+        expect(typeof data === 'object').toBe(true);
+        expect(data).toHaveProperty('title');
+        expect(data.title).toEqual(job.title);
+        expect(data).toHaveProperty('technologies');
+        expect(data.technologies).toEqual(job.technologies);
+        expect(data).toHaveProperty('budget');
+        expect(data.budget).toEqual(job.budget);
+        expect(data).toHaveProperty('description');
+        expect(data.description).toEqual(job.description);
+        expect(data).toHaveProperty('contactEmail');
+        expect(data.contactEmail).toEqual(job.contactEmail);
 
         done();
       })
