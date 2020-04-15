a414fbd00581b13caaaca4a05c76be94d7c14d1a
King Elisha <ek.chibueze@gmail.com>
2020-04-15 21:17:03 +0100

    model update

--- src/models/Job.js
+++ src/models/Job.js
@@ -1,37 +1,40 @@
-const Sequelize = require('sequelize');
-const db = require('../config/db');
+const Sequelize = require("sequelize");
+const db = require("../config/db");
 
-const Job = db.define('job', {
+const Job = db.define("job", {
   title: {
-    type: Sequelize.STRING
+    allowNull: false,
+    type: Sequelize.STRING,
   },
   technologies: {
-    type: Sequelize.STRING
+    allowNull: false,
+    type: Sequelize.STRING,
   },
   description: {
-    type: Sequelize.STRING
+    allowNull: false,
+    type: Sequelize.STRING,
   },
   budget: {
-    type: Sequelize.STRING
+    allowNull: false,
+    type: Sequelize.STRING,
   },
   contactEmail: {
-    type: Sequelize.STRING
-  }
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
 });
 
-if (process.env.NODE_ENV === 'test') {
-  const SequelizeMock = require('sequelize-mock');
+if (process.env.NODE_ENV === "test") {
+  const SequelizeMock = require("sequelize-mock");
   const dbMock = new SequelizeMock();
 
-  const JobMock = dbMock.define('job', {
-    title: 'React',
+  const JobMock = dbMock.define("job", {
+    title: "React",
     technologies: "React, MongoDB",
     budget: 200000,
-    description: 'Build great projects',
-    contactEmail: 'mail@mail.com'
+    description: "Build great projects",
+    contactEmail: "mail@mail.com",
   });
 
   module.exports = JobMock;
-}
-else
-  module.exports = Job;
+} else module.exports = Job;
