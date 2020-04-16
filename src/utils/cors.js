bb268e7d5e6f70cb0b4825d9ce99623da73b5da8
King Elisha <ek.chibueze@gmail.com>
2020-04-16 14:42:21 +0100

    cors

---
+++ src/utils/cors.js
@@ -0,0 +1,17 @@
+const allowCrossDomain = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+module.exports = allowCrossDomain;
