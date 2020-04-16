100adbb476e9434b86756fa1f3d4834bb2af2a54
King Elisha <ek.chibueze@gmail.com>
2020-04-16 10:34:57 +0100

    updated db config

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,12 @@ const { Sequelize } = require("sequelize");
 console.log(process.env.NODE_ENV);
 
 const db =
-  process.env.NODE_ENV === "development"
-    ? new Sequelize(process.env.DB_NAME, "postgres", process.env.DB_PASSWORD, {
+  process.env.NODE_ENV === "production"
+    ? new Sequelize(process.env.DATABASE_URL, {
+        ssl: true,
+      })
+    : new Sequelize(process.env.DB_NAME, "postgres", process.env.DB_PASSWORD, {
         host: "localhost",
         dialect: "postgres",
-      })
-    : new Sequelize(process.env.DATABASE_URL, {
-        ssl: true,
       });
-
 module.exports = db;
