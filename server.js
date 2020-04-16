987039e75c7ada1a557f2e6ce6c035cb49d864c1
King Elisha <ek.chibueze@gmail.com>
2020-04-16 14:43:50 +0100

    integrated cors

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-const express = require('express');
+const express = require("express");
 
-const dotenv = require('dotenv');
-dotenv.config({ path: './src/config/config.env' });
+const dotenv = require("dotenv");
+dotenv.config({ path: "./src/config/config.env" });
 
-const db = require('./src/config/db');
+const db = require("./src/config/db");
+const allowCrossDomain = require("./src/utils/cors");
 
 db.authenticate()
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.log('Error: ' + err));
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("Error: " + err));
 
 const server = express();
 const PORT = process.env.PORT || 8000;
 
+server.use(allowCrossDomain);
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-server.get('/', (_, res) => {
-  res.send('root page');
+server.get("/", (_, res) => {
+  res.send("root page");
 });
 
 // routes
-const jobs = require('./src/routes/jobs');
-server.use('/api/v1/jobs', jobs);
+const jobs = require("./src/routes/jobs");
+server.use("/api/v1/jobs", jobs);
 
-if (process.env.NODE_ENV !== 'test')
+if (process.env.NODE_ENV !== "test")
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = server;
